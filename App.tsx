947d5619851798b1303c1db541c0d3aacb7057fa
Evan Jannot <ejannot@ensc.fr>
2022-02-04 15:58:03 +0100

    Text

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textFont}>Hello World ;)</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textFont:{
+    fontSize:40,
+    color: 'red',
+  }
 });
