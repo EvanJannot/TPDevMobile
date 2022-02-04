51d6510b161c6d209d780d39319120ca6f261624
Evan Jannot <ejannot@ensc.fr>
2022-02-04 16:54:52 +0100

    ScrollView

--- App.tsx
+++ App.tsx
@@ -1,26 +1,24 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.textFont}>Hello World ;)</Text> */}
-      {/* <View style={styles.squareRed}></View>
-      <View style={styles.squareOrange}></View>
-      <View style={styles.squareGreen}></View> */}
-      <Image
-        style={styles.ensc}
-        source={{ uri: 'https://www.bpesquet.fr/images/ENSC.jpg' }}
-      />
-      <Image
-        style={styles.ensc}
-        source={{ uri: 'https://www.bpesquet.fr/images/ENSC.jpg' }}
-      />
-      <Image
-        style={styles.ensc}
-        source={{ uri: 'https://www.bpesquet.fr/images/ENSC.jpg' }}
-      />
-      <StatusBar style="auto" />
+      <ScrollView>
+        <View style={styles.squareV}></View>
+        <ScrollView horizontal>
+          <View style={styles.squareH}></View>
+          <View style={styles.squareH}></View>
+          <View style={styles.squareH}></View>
+        </ScrollView>
+        <View style={styles.squareV}></View>
+        <View style={styles.squareV}></View>
+        <ScrollView horizontal>
+          <View style={styles.squareH}></View>
+          <View style={styles.squareH}></View>
+          <View style={styles.squareH}></View>
+        </ScrollView>
+      </ScrollView>
     </View>
   )
 }
@@ -29,32 +27,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    marginTop: 100,
-    flexDirection: 'column-reverse',
-    alignItems: 'flex-end',
-    justifyContent: 'space-between',
+    flexDirection: 'column',
   },
-  textFont: {
-    fontSize: 40,
-    color: 'red',
+  squareV: {
+    width: 350,
+    height: 300,
+    backgroundColor: 'steelblue',
+    margin: 15,
+    marginLeft: 0,
   },
-  squareRed: {
-    backgroundColor: 'red',
-    height: 100,
-    width: 100,
-  },
-  squareOrange: {
-    backgroundColor: 'orange',
-    height: 100,
-    width: 100,
-  },
-  squareGreen: {
-    backgroundColor: 'green',
-    height: 100,
-    width: 100,
-  },
-  ensc: {
-    width: 108,
-    height: 75,
+  squareH: {
+    width: 300,
+    height: 300,
+    backgroundColor: 'skyblue',
+    margin: 15,
+    marginLeft: 0,
   },
 })
