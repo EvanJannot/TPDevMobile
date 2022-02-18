1e25a4dcfa1f2a899afb20bf4a692bc517b557ac
Evan Jannot <ejannot@ensc.fr>
2022-02-18 11:47:45 +0100

    connexion

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,24 +2,49 @@ import InputContainer from './Input'
 import React, { Component } from 'react'
 import { StyleSheet, View, TouchableOpacity, Text, Alert } from 'react-native'
 
-export default class AuthForm extends Component<{}, {}> {
+interface classState {
+  login: Array<string>
+}
+
+export default class AuthForm extends Component<{}, classState> {
+  state: classState = { login: [] }
   displayAlert = (text: string) => {
     Alert.alert('Action Sélectionnée', text)
   }
+  sendForm = (info: string) => {
+    this.displayAlert('Connexion avec le mail: ' + info)
+  }
+  changeLogin = (value: string) => {
+    let newLogin = [...this.state.login, value]
+    if (this.state.login.length > 2) {
+      this.state.login.splice(0, 2)
+      newLogin = [...this.state.login, value]
+      this.setState({ login: newLogin })
+    } else {
+      this.setState({ login: newLogin })
+    }
+  }
+
   render() {
     return (
       <View style={styles.form}>
         <InputContainer
+          security={false}
           textForm="email"
           imagePath={require('../assets/at.png')}
+          modify={this.changeLogin}
         />
         <InputContainer
+          security={true}
           textForm="mot de passe"
           imagePath={require('../assets/key.png')}
+          modify={this.changeLogin}
         />
         <TouchableOpacity
           style={styles.button}
-          onPress={() => this.displayAlert(`Connexion avec le mail : `)}
+          onPress={() =>
+            this.sendForm(this.state.login[0] + '\n' + this.state.login[1])
+          }
         >
           <Text style={styles.textButton}>Se connecter</Text>
         </TouchableOpacity>
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react-native'
 import React, { Component } from 'react'
-import { StyleSheet, TextInput, View, Image } from 'react-native'
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Image,
+  ImageSourcePropType,
+} from 'react-native'
 
 interface CLassPropsType {
   textForm: string
-  imagePath: string
+  imagePath: ImageSourcePropType
+  modify: Function
+  security: boolean
 }
 
 export default class InputContainer extends Component<CLassPropsType, {}> {
@@ -13,8 +21,12 @@ export default class InputContainer extends Component<CLassPropsType, {}> {
       <View style={styles.logoinput}>
         <Image style={styles.logo} source={this.props.imagePath} />
         <TextInput
+          secureTextEntry={this.props.security}
           style={styles.input}
           placeholder={this.props.textForm}
+          onSubmitEditing={(event) => {
+            this.props.modify(event.nativeEvent.text)
+          }}
         ></TextInput>
       </View>
     )
