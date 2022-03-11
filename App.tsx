1853e0b81bc8a2284b838d1f0d7182915ab4ea42
Evan Jannot <ejannot@ensc.fr>
2022-03-11 15:17:08 +0100

    First commit

--- App.tsx
+++ App.tsx
@@ -9,33 +9,12 @@ import {
   TouchableOpacity,
   Alert,
 } from 'react-native'
-import AuthForm from './components/AuthForm'
-import { User } from './services/authentication.service'
-import ModuleList from './components/ModuleList'
 
-interface classState {
-  currentUser: User | null
-}
-
-export default class App extends Component<{}, classState> {
-  state: classState = { currentUser: null }
-  displayAlert = (text: string) => {
-    Alert.alert('Action Sélectionnée', text)
-  }
-
-  connect = (loggedUser: User) => {
-    this.setState({ currentUser: loggedUser })
-  }
-
+export default class App extends Component<{}, {}> {
   render() {
-    const logged = !this.state.currentUser ? (
-      <AuthForm onConnection={this.connect} />
-    ) : null
-    const modules = this.state.currentUser ? <ModuleList /> : null
     return (
       <View style={styles.container}>
-        {logged}
-        {modules}
+        <Text>Salut</Text>
       </View>
     )
   }
@@ -48,10 +27,5 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     marginTop: 40,
-  },
-  koala: {
-    width: 140,
-    height: 120,
-    marginBottom: 25,
   },
 })
